b9863bd2289d75f6650feaefaaa82a5ac1989f19
Danny <danyl1607@gmail.com>
2022-09-22 23:51:56 +0300

    add readonly attribute & drawer

---
+++ Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Polyternity.Editor
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var previousGUIState = GUI.enabled;
+            
+            GUI.enabled = false;
+
+            PropertyDrawerFinder.FindDrawerForProperty(property).OnGUI(position, property, label);
+            
+            GUI.enabled = previousGUIState;
+        }
+    }
+
+}
---
+++ Runtime/ReadOnlyAttribute.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+namespace Polyternity
+{
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
